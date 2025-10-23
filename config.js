7c123cd8a8caf195393170e01b68c4df6bc5ee0b
azhamsukaloli <raffa.farrel1@gmail.com>
2025-10-23 15:51:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+//credit By Limzzzhama 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "http://xillobat.private.naell.my.id", // Ubah jadi Domain panel Mu !!!
+  port: "3172" // Ubah Jadi Port Panel Mu !!!
+};
