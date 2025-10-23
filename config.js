fd0e1fa10d088ae7ad812c84dc0126597ea1e73f
azhamsukaloli <raffa.farrel1@gmail.com>
2025-10-23 16:02:42 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-//credit By Limzzzhama 
-//no hapus credit ketauan hapus hitam//
-
-module.exports = {
-  domain: "https://ditz-yantoo.naell.my.id/", // Ubah jadi Domain panel Mu !!!
-  port: "3172" // Ubah Jadi Port Panel Mu !!!
-};
